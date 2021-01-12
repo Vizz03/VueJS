2ce6f89ec659ce6dc3dffc86703bd0b5f92bea33
Tinashe "Viz" Matembo <31240908+Vizz03@users.noreply.github.com>
2021-01-13 00:14:48 +0200

    Create App.vue

---
+++ URL-Shortener/client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">URL Shortener</a>
+    </nav>
+    <div class="container">
+     <home />
+    </div>
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+export default {
+  name: "App",
+  components: {
+    Home,
+  },
+};
+</script> 
