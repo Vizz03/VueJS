718cca4aabd9b43c7fc7fcaa4171bef98b20aca0
Tinashe "Viz" Matembo <31240908+Vizz03@users.noreply.github.com>
2022-01-18 12:26:48 +0200

    customer crud app

---
+++ Vue-CRUD-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <header-bar></header-bar>
+    <b-container>
+      <b-row class="mt-5">
+        <router-view />
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import HeaderBar from "@/components/HeaderBar.vue";
+export default {
+ components: {
+    HeaderBar,
+  },
+}
+</script>
+
+<style>
+
+</style>
